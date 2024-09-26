d48f34219ee1df748b69abe4e104cf072a8f39bd
Jesus M. Lara <jesus.lara@alight.com>
2024-09-26 18:02:18 -0500

    Testng the Data Contract

---
+++ src/main/java/example/cashcard/CardcashApplication.java
@@ -0,0 +1,13 @@
+package example.cashcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardcashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardcashApplication.class, args);
+	}
+
+}
